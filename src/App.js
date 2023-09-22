e6062901d0fee44c9508adc2494239235350788e
iskry <refocuslucifer@protonmail.com>
2023-09-22 14:30:06 -0500

    created a header component. replaced header components in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Container, Grid, Header, Segment, Statistic, Icon, Form, Button} from 'semantic-ui-react';
 import './App.css';
+import MainHeader from './components/MainHeader';
 
 function App() {
   return (
 
       <Container>
-        <Header as='h1'>Budget</Header>
+        <MainHeader title='Budget'/>
         <Statistic size='small'>
         <Statistic.Label>Your Balance:</Statistic.Label>
         <Statistic.Value>2,550.53</Statistic.Value>
@@ -36,8 +37,8 @@ function App() {
                 </Grid.Row>
             </Grid>
         </Segment>
-
-      <Header as='h3'>History</Header>
+      
+      <MainHeader title='History' type='h3'/>
       <Segment color='red'>
         <Grid columns={3} textAlign='right'>
           <Grid.Row>
@@ -75,7 +76,7 @@ function App() {
         </Grid>
       </Segment>
 
-      <Header as='h3'>Add New Transaction</Header>
+      <MainHeader title='Add New Transaction' type='h3'/>
       <Form unstackable>
         <Form.Group>
           <Form.Input icon='tags' width={12} label='Description' placeholder='New Shiny Thing' />
