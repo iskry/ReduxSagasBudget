83547205d56760648a0d6f1e8489f17982a4f012
iskry <refocuslucifer@protonmail.com>
2023-09-22 19:42:42 -0500

    created a entrylines.js component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Container }  from 'semantic-ui-react';
 import './App.css';
 import MainHeader from './components/MainHeader';
@@ -5,8 +6,10 @@ import NewEntryForm from './components/NewEntryForm';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
 import EntryLine from './components/EntryLine';
+import EntryLines from './components/EntryLines';
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
   return (
 
       <Container>
@@ -14,9 +17,7 @@ function App() {
         <DisplayBalance title='Your Balance' value='2,550.53' size='small' />
         <DisplayBalances />
         <MainHeader title='History' type='h3'/>
-        <EntryLine description='Something' value='$10.00' isExpense='true'/>
-        <EntryLine description='Something Else' value='$100.00' />
-        <EntryLine description='Something' value='$20.00' isExpense='true'/>
+        <EntryLines entries={entries}/>
         <MainHeader title='Add New Transaction' type='h3'/>
         <NewEntryForm />
       </Container>
@@ -25,3 +26,26 @@ function App() {
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    description: 'Work Income',
+    value: '$1000.00',
+    isExpense: false
+  },
+  {
+    description: 'Water Bill',
+    value: '$20.00',
+    isExpense: true
+  },
+  {
+    description: 'Rent',
+    value: '$300.00',
+    isExpense: true
+  },
+  {
+    description: 'Power Bill',
+    value: '$50.00',
+    isExpense: true
+  },
+]
