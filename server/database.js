ab85a627c76fd56429e89988f1ddcada129c5f2a
iskry <refocuslucifer@protonmail.com>
2023-09-25 12:45:25 -0500

    added server file structure. created Entry point for Sequelize

---
+++ server/database.js
@@ -0,0 +1,20 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const Entry = sequelize.define('Entry', {
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  value: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  isExpense: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+});
+
+module.exports = Entry;
+
