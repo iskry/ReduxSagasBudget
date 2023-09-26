565b8dce1cb8de6a9186bd875d2efad0e7bbe950
iskry <refocuslucifer@protonmail.com>
2023-09-25 21:15:59 -0500

    added a default state to reducer while transitioning to react-redux

--- client/src/reducers/entries.reducers.js
+++ client/src/reducers/entries.reducers.js
@@ -1,11 +1,11 @@
-const reducer = (state = entries, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_ENTRY":
-      if (action.payload) state = [...state, action.payload];
+      if (action.payload) return [...state, action.payload];
       break;
     case "REMOVE_ENTRY":
       if (action.payload && action.payload.id)
-        state = state.filter((entry) => entry.id !== action.payload.id);
+        return state.filter((entry) => entry.id !== action.payload.id);
       break;
     default:
       return state;
